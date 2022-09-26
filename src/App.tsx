fed5d27e18bc52b27774b54f2aea3a89b5c2d408
Alex Stoicuta <alex.stoicuta@123formbuilder.com>
2022-09-26 14:13:53 +0300

    Added basic reveal transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="box">
+
+        <div className="title" >
+          <span className="block" ></span>
+          <h1>Alex Stoicuta<span></span></h1>
+        </div >
+
+        <div className="role">
+          <div className="block" ></div>
+          <p>Frontend Engineer</p>
+        </div>
+
+      </div>
     </div>
   );
 }
